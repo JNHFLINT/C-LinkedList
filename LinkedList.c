1993c0aecc7218a136de18c7b17b7c1431be384c
JNHFLINT <123217021+JNHFLINT@users.noreply.github.com>
2025-10-01 03:25:45 +0000

    Added necessary files

---
+++ LinkedList.c
@@ -0,0 +1,8 @@
+#include "LinkedList.h"
+
+struct Node* createNode(char* data);
+
+struct Node* createNode(char* data) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+}
