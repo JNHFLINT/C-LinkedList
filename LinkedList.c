3fc0e27dfe88ec713107177d8846c9d860ef6fa3
JNHFLINT <123217021+JNHFLINT@users.noreply.github.com>
2025-10-01 03:47:48 +0000

    Implemented createNode function

--- LinkedList.c
+++ LinkedList.c
@@ -1,8 +1,27 @@
 #include "LinkedList.h"
+#include <stdlib.h>
+#include <string.h>
+
 
 struct Node* createNode(char* data);
+void insertAtEnd(struct Node** head, struct Node* newNode);
 
 struct Node* createNode(char* data) {
-    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
-    
+    struct Node *newNode = malloc(sizeof(struct Node));
+    // malloc check
+    if(newNode == NULL) {
+        return NULL;
+    }
+    newNode -> data = (char *)malloc(strlen(data) + 1);
+    // check again
+    if(newNode -> data == NULL) {
+        free(newNode);
+        return NULL;
+    }
+
+
+    strcpy(newNode -> data, data);
+    newNode -> next = NULL;
+
+    return newNode;
 }
